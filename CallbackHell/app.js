e241f26f18c9357f5e89f8de1a7ea037afa0a0cc
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-01-30 20:14:56 +0530

    callback hell- changing background color of page with slight transition

---
+++ CallbackHell/app.js
@@ -0,0 +1,38 @@
+// setTimeout(() =>{
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() =>{
+//     document.body.style.backgroundColor = 'orange';
+//     setTimeout(() =>{
+//     document.body.style.backgroundColor = 'yellow';
+//     setTimeout(() =>{
+//     document.body.style.backgroundColor = 'green';
+//     setTimeout(() =>{
+//     document.body.style.backgroundColor = 'blue';
+//     setTimeout(() =>{
+//     document.body.style.backgroundColor = 'violet';
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+// },1000)
+
+const delayedColorChange = (color,delay,doNext)=> {
+setTimeout(() =>{
+  document.body.style.backgroundColor = color;
+  doNext && doNext();
+},delay)
+}
+
+delayedColorChange('red',1000,() =>{
+    delayedColorChange('orange',1000,() =>{
+    delayedColorChange('yellow',1000,() =>{
+    delayedColorChange('green',1000,() =>{
+    delayedColorChange('blue',1000,() =>{
+    delayedColorChange('purple',1000,() =>{
+})
+})
+})
+})
+})
+})
