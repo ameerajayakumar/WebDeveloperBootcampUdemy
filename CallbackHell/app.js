3bef04a790625e44d0f7454a296121cb4f8f7560
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-01-31 17:27:22 +0530

    updating delayedbgcolor with async and await

--- CallbackHell/app.js
+++ CallbackHell/app.js
@@ -46,10 +46,30 @@ const delayedColorChange = (color,delay) => {
   })
 }
 
-delayedColorChange('red', 1000)
-.then(() => delayedColorChange('orange', 1000))
-.then(() => delayedColorChange('yellow', 1000))
-.then(() => delayedColorChange('green', 1000))
-.then(() => delayedColorChange('blue', 1000))
-.then(() => delayedColorChange('indigo', 1000))
-.then(() => delayedColorChange('purple', 1000))
+// delayedColorChange('red', 1000)
+// .then(() => delayedColorChange('orange', 1000))
+// .then(() => delayedColorChange('yellow', 1000))
+// .then(() => delayedColorChange('green', 1000))
+// .then(() => delayedColorChange('blue', 1000))
+// .then(() => delayedColorChange('indigo', 1000))
+// .then(() => delayedColorChange('purple', 1000))
+
+async function rainbow(){
+  await delayedColorChange('red', 1000)
+  await delayedColorChange('orange', 1000)
+  await delayedColorChange('yellow', 1000)
+  await delayedColorChange('green', 1000)
+  await delayedColorChange('blue', 1000)
+  await delayedColorChange('indigo', 1000)
+  await delayedColorChange('purple', 1000)
+
+}
+
+// rainbow().then(() => { console.log("End of rainbow!"); })
+
+async function printRain(){
+  await rainbow();
+  console.log("End of rainbow!");
+}
+
+printRain();
