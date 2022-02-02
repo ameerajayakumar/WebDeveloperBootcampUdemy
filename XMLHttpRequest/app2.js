f3df48575502e6e21e3f901a1229524a22c5ba3f
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-02-02 21:22:02 +0530

    axios baddadjokes example

--- XMLHttpRequest/app2.js
+++ XMLHttpRequest/app2.js
@@ -6,13 +6,30 @@
 //     console.log(e);
 // })
 
-const fetchBitcoinPrice = async () => {
-    try{
-    const res = await axios.get("https://api.cryptonator.com/api/ticker/btc-usd");
-    console.log("PRICE ", res.data.ticker.price);
-    }
-    catch(e){
-        console.log("ERROR CAUGHT IS: ", e)
-    }
+// const fetchBitcoinPrice = async () => {
+//     try{
+//     const res = await axios.get("https://api.cryptonator.com/api/ticker/btc-usd");
+//     console.log("PRICE ", res.data.ticker.price);
+//     }
+//     catch(e){
+//         console.log("ERROR CAUGHT IS: ", e)
+//     }
+// }
+// fetchBitcoinPrice();
+
+//click button to get new jokes & add them to a list
+const addNewJoke = async () => {
+    const jokeText = await getDadJokes();
+    const newLi = document.createElement("LI");
+    newLi.append(jokeText);
+    list.append(newLi); 
 }
-fetchBitcoinPrice();
+const getDadJokes = async () => {
+     const config = { headers: {Accept: 'application/json' } };
+     const joke = await axios.get("https://icanhazdadjoke.com/",config);
+     return joke.data.joke;
+    
+}
+const list = document.querySelector("#jokes");
+const button = document.querySelector("button");
+button.addEventListener("click", addNewJoke);
