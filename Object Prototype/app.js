78f9b89eeea25aaebce2ac76ce2f60026a67976d
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-02-07 20:50:05 +0530

    object prototype example two

--- Object Prototype/app.js
+++ Object Prototype/app.js
@@ -3,3 +3,7 @@ String.prototype.yell = function() {
     return `OMGGGG !! ${this.toUpperCase()}!!!! ARGGHHH`; 
     //"luna & halo".yell() --> 'OMGGGG !! LUNA & HALO!!!! ARGGHHH'
 } 
+
+Array.prototype.pop = function() {
+    return 'NO WAY! I WANT TO KEEP THIS ELEMENT!';
+}
