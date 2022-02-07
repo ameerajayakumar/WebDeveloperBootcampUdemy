24cc88e0612b84a64530c876e9e66fbae532511a
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-02-07 20:47:08 +0530

    object prototype example

---
+++ Object Prototype/app.js
@@ -0,0 +1,5 @@
+String.prototype.yell = function() {
+    //console.log(this.toUpperCase());
+    return `OMGGGG !! ${this.toUpperCase()}!!!! ARGGHHH`; 
+    //"luna & halo".yell() --> 'OMGGGG !! LUNA & HALO!!!! ARGGHHH'
+} 
