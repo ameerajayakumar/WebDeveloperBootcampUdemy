54694193bba40f4dae99018f3a50997fd5ab724f
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-02-08 14:47:05 +0530

    object prototype example two- comments

--- Object Prototype/app.js
+++ Object Prototype/app.js
@@ -2,8 +2,13 @@ String.prototype.yell = function() {
     //console.log(this.toUpperCase());
     return `OMGGGG !! ${this.toUpperCase()}!!!! ARGGHHH`; 
     //"luna & halo".yell() --> 'OMGGGG !! LUNA & HALO!!!! ARGGHHH'
-} 
+}; 
 
 Array.prototype.pop = function() {
     return 'NO WAY! I WANT TO KEEP THIS ELEMENT!';
-}
+};
+
+/*
+Prototype is the template object(Arrays) which contains some properties/methods
+String.prototype is the actual String prototype object and __proto__ is the reference to this String prototype
+*/
