1f8984f52916cd6222e5a988ffc4b454777cac1a
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-02-09 17:43:18 +0530

    adding

--- Nodejs/fileSystem.js
+++ Nodejs/fileSystem.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
-fs.mkdir('Dogs', { recursive: true }, (err) => {
-    console.log("inside callback")
-  if (err) throw err;
-});
-console.log("After MKDIR")
+const folderName = process.argv[2] || 'Project';
+// fs.mkdir('Dogs', { recursive: true }, (err) => {
+//     console.log("inside callback")
+//   if (err) throw err;
+// });
+try{
+fs.mkdirSync(folderName); 
+fs.writeFileSync(`${folderName}/index.html`, '')
+fs.writeFileSync(`${folderName}/app.css`, '')
+fs.writeFileSync(`${folderName}/app.js`, '')
+console.log("After MKDIR");
+}
+catch(e){
+    console.log(e);
+}
